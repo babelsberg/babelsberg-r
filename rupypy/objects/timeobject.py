f95a12da83aa0d57b0bfc25a924fa0132311a5ca
Tim Felgentreff <timfelgentreff@gmail.com>
2012-08-07 10:50:58 +0200

    pypy rpython time functions

--- rupypy/objects/timeobject.py
+++ rupypy/objects/timeobject.py
@@ -1,9 +1,47 @@
+import os
 import time
+
+from pypy.module.rctime.interp_time import c_localtime, c_mktime, cConfig
+from pypy.rpython.lltypesystem import rffi
+from pypy.rpython.lltypesystem import lltype
 
 from rupypy.module import ClassDef
 from rupypy.objects.objectobject import W_Object
 from rupypy.objects.floatobject import W_FloatObject
 from rupypy.objects.intobject import W_FixnumObject
+
+def _tm_to_ary(t):
+    return [
+        rffi.getintfield(t, 'c_tm_year') + 1900,
+        rffi.getintfield(t, 'c_tm_mon') + 1, # want january == 1
+        rffi.getintfield(t, 'c_tm_mday'),
+        rffi.getintfield(t, 'c_tm_hour'),
+        rffi.getintfield(t, 'c_tm_min'),
+        rffi.getintfield(t, 'c_tm_sec'),
+        (rffi.getintfield(t, 'c_tm_wday') + 6) % 7, # want monday == 0
+        rffi.getintfield(t, 'c_tm_yday') + 1, # want january, 1 == 1
+        rffi.getintfield(t, 'c_tm_isdst')
+    ]
+
+def _localtime(seconds):
+    t = rffi.r_time_t(seconds)
+    t_ref = lltype.malloc(rffi.TIME_TP.TO, 1, flavor='raw')
+    t_ref[0] = seconds
+    p = c_localtime(t_ref)
+    lltype.free(t_ref, flavor='raw')
+    return _tm_to_ary(p)
+
+def _mktime(year, month, mday, hour, minute, second):
+    tm_buf = lltype.malloc(cConfig.tm, flavor='raw', zero=True)
+    rffi.setintfield(tm_buf, 'c_tm_year', year)
+    rffi.setintfield(tm_buf, 'c_tm_mon', month)
+    rffi.setintfield(tm_buf, 'c_tm_mday', mday)
+    rffi.setintfield(tm_buf, 'c_tm_hour', hour)
+    rffi.setintfield(tm_buf, 'c_tm_min', minute)
+    rffi.setintfield(tm_buf, 'c_tm_sec', second)
+    tt = c_mktime(tm_buf)
+    lltype.free(tm_buf, flavor='raw')
+    return float(tt)
 
 
 class W_TimeObject(W_Object):
@@ -21,15 +59,15 @@ class W_TimeObject(W_Object):
     def singleton_method(self, space, w_sec, w_usec=None):
         if isinstance(w_sec, W_TimeObject):
             args = w_sec.time_struct[:5]
-            epoch = w_sec.epoch % 60
+            epoch = int(w_sec.epoch) % 60
         else:
             if w_usec:
                 usec = space.float_w(w_usec) / 1000
             else:
                 usec = 0.0
             sec = int(space.float_w(w_sec))
-            args = time.localtime(sec)[:5]
-            epoch = (sec + usec) % 60
+            args = _localtime(sec)[:5]
+            epoch = int(sec + usec) % 60
         args_w = [space.newint(i) for i in args]
         args_w.append(space.newfloat(epoch))
         return space.send(space.getclassfor(W_TimeObject), space.newsymbol("new"), args_w)
@@ -38,22 +76,20 @@ class W_TimeObject(W_Object):
     def method_initialize(self, space, args_w):
         if not args_w:
             self.epoch = time.time()
-            self.time_struct = time.localtime(self.epoch)
+            self.time_struct = _localtime(int(self.epoch))
         else:
             if len(args_w) > 6:
                 raise NotImplementedError("Time.new with utc_offset")
             args = [space.float_w(i) for i in args_w]
             args += [0] * (6 - len(args))
-            struct = time.strptime(
-                "%04d %02d %02d %02d %02d %02d" % tuple(args),
-                "%Y %m %d %H %M %S"
-            )
-            self.time_struct = struct
-            self.epoch = time.mktime(struct) + (args[5] % 1)
+            self.epoch = _mktime(
+                args[0], args[1], args[2], args[3], args[4], args[5]
+            ) + (args[5] % 1)
+            self.time_struct = _localtime(self.epoch)
 
     @classdef.method("to_s")
     def method_to_s(self, space):
-        return space.newstr_fromstr(time.strftime("%Y-%m-%d %H:%M:%S %z", self.time_struct))
+        return space.newstr_fromstr("%d-%d-%d %d:%d:%d" % self.time_struct[:6])
 
     @classdef.method("to_f")
     def method_to_f(self, space):
