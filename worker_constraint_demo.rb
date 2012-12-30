7252ab2ff582fa0f2716986a690a846ec523193b
Tim Felgentreff <timfelgentreff@gmail.com>
2012-12-30 22:28:20 +0100

    tests

--- worker_constraint_demo.rb
+++ worker_constraint_demo.rb
@@ -1,31 +1,45 @@
+# -*- coding: undecided -*-
 
-class Task
-  def initialize(a)
-    @a = a
-    @is_ready = true
-  end
+# class Task
+#   def initialize(a)
+#     @a = a
+#     @is_ready = true
+#   end
 
-  def run
-    puts @a
-    @is_ready = false
-  end
+#   def run
+#     puts @a
+#     @is_ready = false
+#   end
 
-  def ready?
-    @is_ready
-  end
-end
+#   def ready?
+#     @is_ready
+#   end
+# end
 
-tasks = [1..15].map do |num|
-  Task.new(num)
-end
+# tasks = [1..15].map do |num|
+#   Task.new(num)
+# end
 
-task = tasks[0]
+# task = tasks[0]
 
-c = Constraint.new(10) do
-  task.ready?
-end
-c.enable
+# c = Constraint.new(10) do
+#   task.ready?
+# end
+# c.enable
 
-while true
-  task.run
-end
+# while true
+#   task.run
+# end
+
+# class User
+#   def initialize(birthyear, age)
+#     @birthyear = birthyear
+#     @age = age
+#     constrain(?@age == 2013 - @birthyear)
+#   end
+# end
+
+# u = User.new(2012, 11)
+@age = 24
+@birthyear = 1988
+puts(constrain: @age == 2013 - @birthyear)
