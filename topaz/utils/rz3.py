45e51a16a29a76670064c549f361f274a67a69ac
Tim Felgentreff <timfelgentreff@gmail.com>
2013-02-14 12:40:41 +0100

    compile with z3

--- topaz/utils/rz3.py
+++ topaz/utils/rz3.py
@@ -12,7 +12,7 @@ POSIX = os.name == "posix"
 
 
 class CConfig:
-    includes = ["rffi_z3.h"]
+    includes = ["z3.h"]
 
     z3_dir = os.path.abspath(os.path.join(
         os.path.dirname(__file__),
@@ -21,7 +21,9 @@ class CConfig:
         "dependencies",
         "z3"
     ))
-    z3_exe = os.path.join(z3_dir, "build", "z3")
+    z3_include_path = os.path.join(z3_dir, "src", "api")
+    z3_build_path = os.path.join(z3_dir, "build")
+    z3_exe = os.path.join(z3_build_path, "z3")
     if WINNT:
         z3_exe += ".exe"
 
@@ -45,11 +47,13 @@ class CConfig:
 
     eci = ExternalCompilationInfo(
         includes=includes,
-        include_dirs=[
-            onig_path,
-            os.getcwd() + "/rupypy/utils/re"
-        ],
-        libraries=["onig"],
-        library_dirs=[onig_lib_path],
-        separate_module_files=["rupypy/utils/re/rffi_helper.c"]
+        include_dirs=[z3_include_path],
+        libraries=["z3"],
+        library_dirs=[onig_build_path]
+        # separate_module_files=["rupypy/utils/re/rffi_helper.c"]
     )
+
+    if WINNT:
+        calling_conv = "win"
+    else:
+        calling_conv = "c"
