3ae8489a69ba4e00437be887f5b70dd581aab7b5
Tim Felgentreff <timfelgentreff@gmail.com>
2013-05-30 17:48:46 +0200

    run animal puzzle

--- tests/test_constraintexamples.py
+++ tests/test_constraintexamples.py
@@ -40,3 +40,13 @@ Spend exactly 100 dollars and buy exactly 100 animals. Dogs cost
 have to buy at least one of each. How many of each should you buy?
 Dogs: 3, cats: 41, mice: 56
 """
+
+    def test_animal_puzzle(self, space, tmpdir, capfd):
+        self.run(space, tmpdir, "animal_puzzle.rb")
+        out, err = capfd.readouterr()
+        assert out == u"""Z3 constraint solver loaded.
+Spend exactly 100 dollars and buy exactly 100 animals. Dogs cost
+15 dollars, cats cost 1 dollar, and mice cost 25 cents each. You
+have to buy at least one of each. How many of each should you buy?
+Dogs: 3, cats: 41, mice: 56
+"""
