90f4634c03c6ca0f18c21a164f0b918bfd051908
johenning <johannes-henning@gmx.de>
2014-06-09 17:27:26 +0200

    work session 09.06

---
+++ bin/test.rb
@@ -0,0 +1,21 @@
+require "libz3"
+
+colours = [:Black, :Brown, :Blue, :White]
+domain {shoes.in colours}
+domain {shirt.in colours}
+domain {pants.in colours}
+domain {hat.in colours}
+
+always {shoes == hat}
+always {shoes != pants}
+always {shoes != shirt}
+always {shirt != pants}
+
+always {shoes == :Brown | shoes == :Black}
+always {shirt == :Brown | shirt == :Blue | shirt == :White}
+always {hat == :Brown}
+
+print "shirt:#{shirt}"
+print "shoes:#{shoes}"
+print "hat:#{hat}"
+print "pants:#{pants}"
