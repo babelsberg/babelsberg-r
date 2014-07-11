6f0d378113b907cedfa49af869bf92aa516a8a48
johenning <johannes-henning@gmx.de>
2014-07-11 18:26:30 +0200

    Fixed nil behaviour and direct variable access

--- bin/test.rb
+++ bin/test.rb
@@ -1,27 +1,33 @@
 require "libz3"
 
-shoes, hat, shirt, pants = [0]*4
+class Symbol
+  def constraint_solver
+    Z3::Instance
+  end
+end
 
 colours = [:Blue, :Black, :Brown, :White]
-always {shoes.in colours}
-always {shirt.in colours}
-always {pants.in colours}
-always {hat.in colours}
+always {@shoes.in colours}
+always {@shirt.in colours}
+always {@pants.in colours}
+always {@hat.in colours}
 
-always {shoes == hat}
-always {shoes != pants}
-always {shoes != shirt}
-always {shirt != pants}
+always {@shoes == @hat}
+always {@shoes != @pants}
+always {@shoes != @shirt}
+always {@shirt != @pants}
 
 # Alternative
 # always { (shoes == :Brown) or (shoes == :Black) }
 # always { ((shirt == :Brown) or (shirt == :Blue)) or (shirt == :White) }
+@shoes = :White
+puts @shoes, @hat, @pants, @shirt
 
-always { hat == :Brown }
-always { shoes.one_of :Brown, :Black }
-always { shirt.one_of :White, :Blue, :Brown }
+always { @hat == :Brown }
+always { @shoes.one_of :Brown, :Black }
+always { @shirt.one_of :White, :Blue, :Brown }
 
-print "shirt:\t#{shirt}\n"
-print "shoes:\t#{shoes}\n"
-print "hat:\t#{hat}\n"
-print "pants:\t#{pants}\n"
+print "shirt:\t#{@shirt}\n"
+print "shoes:\t#{@shoes}\n"
+print "hat:\t#{@hat}\n"
+print "pants:\t#{@pants}\n"
